0760381bb3ec3ce89ae1da2de4f1df09fa0e68df
Andy Feng <anndyfeng1@gmail.com>
2016-04-01 13:57:36 -0700

    second commit

--- config/auth.js
+++ config/auth.js
@@ -6,13 +6,13 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1797954803765562', // your App ID
         'clientSecret'  : '0b9e2c704a68b39df28011d5dedd993c', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+        'callbackURL'   : 'http://www.hamster.gold/auth/facebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'       : 'FzmwKA0e7BR0rOvrsSDvzNVIH',
         'consumerSecret'    : 'VBloMLvqABilyxyBrEtUWwOZq1TtxPxHKOFdIl6VRducFA56Ul',
-        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+        'callbackURL'       : 'http://www.hamster.gold/auth/twitter/callback'
     }
 
 };
